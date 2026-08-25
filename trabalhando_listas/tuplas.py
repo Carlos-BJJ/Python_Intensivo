a0d376ae76a12b73e99e170ea2f11f1a37d2a5e1
CarlosPereira <carlos.pereira21ofc@gmail.com>
2026-08-24 22:07:43 -0300

    tuplas.py: Adicionei informações sobre tuplas

---
+++ trabalhando_listas/tuplas.py
@@ -0,0 +1,7 @@
+#Definindo uma tupla (Lista imutavel)
+
+dimensoes = (200, 50)
+print(dimensoes[0])
+
+dimensoes = (400, 100) #Não posso modificar a tupla, porém posso mudar a tupla atribuida a variavel
+print(dimensoes[0])
